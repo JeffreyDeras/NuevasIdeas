242b21f23f6e0a044c14ccaa2daa42cfb575af40
jeffreyderas <jefreyjb10@gmail.com>
2022-09-22 12:34:49 -0300

    Versão 1.1

---
+++ script.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+            if(this.scrollY > 500){
+                $('.scroll-up-btn').addClass("show");
+            }else{
+                $('.scroll-up-btn').removeClass("show");
+            }    
+        }); 
+        
+        $('.scroll-up-btn').click(function(){
+            $('html').animate({scrollTop: 0});
+        });
+
+        var typed = new Typed(".typing", {
+            strings:[""],
+            typeSpeed:25,
+            backSpeed:25,
+            loop:true
+        });
+        var typed = new Typed(".typing-2", {
+           strings:["Abogado Ejemplo"],
+           typeSpeed:100,
+           backSpeed:100,
+           loop:true
+       });
+
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
+
+$('.carousel').owlCarousel({
+    margin:20,
+    loop:true,
+    autoplayTime:2000,
+    autoplayHoverPauser:true,
+    responsive:{
+        0:
+        {
+            items:1,
+            nav:false
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:false,
+        }
+    }
+
+});
+
+});
